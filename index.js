e3ee5a7fc036f2d573c7487812c618a88d2893d1
chakal05 <chakirrasta@gmail.com>
2019-07-08 17:58:15 +0200

    test2

--- index.js
+++ index.js
@@ -19,5 +19,11 @@ app.use('/requests/loadHours', loadHours);
 app.use('/requests/register', register);
 
 
+// Static folder
+app.use(express.static(__dirname + '/public/'));
+// SPA
+app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+
 const port = process.env.port || 5000;
 app.listen(port, () => console.log("app running on port 5000"));
